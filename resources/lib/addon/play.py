01d4246be1d4675a356fde8b420466f73d6d1bcc
Adam Malcontenti-Wilson <adman.com@gmail.com>
2014-06-09 16:29:10 +1000

    Fix up imports

--- resources/lib/addon/play.py
+++ resources/lib/addon/play.py
@@ -2,17 +2,17 @@
 #   Network Ten CatchUp TV Video Addon
 #
 #   Copyright (c) 2013 Adam Malcontenti-Wilson
-# 
+#
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
 #   of this software and associated documentation files (the "Software"), to deal
 #   in the Software without restriction, including without limitation the rights
 #   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 #   copies of the Software, and to permit persons to whom the Software is
 #   furnished to do so, subject to the following conditions:
-# 
+#
 #   The above copyright notice and this permission notice shall be included in
 #   all copies or substantial portions of the Software.
-# 
+#
 #   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 #   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,20 +24,17 @@
 
 import sys
 import urllib
-import xbmc
-import xbmcgui
-import xbmcplugin
 import utils
 from networktenvideo.api import NetworkTenVideo, SWF_URL, PAGE_URL
 
 import xbmcswift2
-from xbmcswift2 import ListItem
+from xbmcswift2 import xbmc, xbmcgui, xbmcplugin, ListItem
 from apicache import APICache
 
 class Module(xbmcswift2.Module):
   def __init__(self):
     super(Module, self).__init__('plugin.video.catchuptv.au.ten.play')
-    
+
     # decorators
     self.play = self.route('/play/<videoId>')(self.play)
 
@@ -46,7 +43,8 @@ class Module(xbmcswift2.Module):
     media = api.get_media_for_video(videoId)
     self.log.debug('Found media renditions for video: %s', repr(media.items))
 
-    # Blindly go for the highest bitrate for now. Later versions could include a customisable setting of which stream to use
+    # Blindly go for the highest bitrate for now.
+    # Later versions could include a customisable setting of which stream to use
     media_sorted = sorted(media.items, key=lambda m: m.encodingRate, reverse=True)
     media = media_sorted[0]
     path = media.defaultURL
@@ -54,7 +52,7 @@ class Module(xbmcswift2.Module):
       path = path.replace('&mp4:', ' playpath=mp4:')
       path += ' swfVfy=true swfUrl=%s pageUrl=%s' % (SWF_URL, PAGE_URL)
     self.log.info('Using rendition: %s with url: %s' % (media, path))
-    
+
     # Set the resolved url, and include media stream info
     item = ListItem.from_dict(path=path)
     item.add_stream_info('video', {'codec': media.videoCodec, 'width': media.frameWidth, 'height': media.frameHeight})
