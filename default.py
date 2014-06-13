ccb1c6e31a1218f8544f13518f775c4337c665b1
Adam Malcontenti-Wilson <adman.com@gmail.com>
2014-06-13 20:41:51 +1000

    Rename addon to prevent conflicts

--- default.py
+++ default.py
@@ -29,13 +29,11 @@ try:
 except:
    current_dir = os.getcwd()
 
-# Temporary logging to track down import errors
-print "[TENPLAY ADDON] Adding \"%s\" to import lookup path" % os.path.join(current_dir, 'resources', 'lib')
 sys.path.append(os.path.join(current_dir, 'resources', 'lib'))
 
 from xbmcswift2 import Plugin
 from utils import Utils
-from addon import Addon
+from networktenaddon import Addon
 
 plugin = Plugin()
 utils = Utils(plugin)
